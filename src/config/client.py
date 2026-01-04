211e483a9998dadc1608edbd926cb0e88481983d
MathieuAudibert <mathieu.audibert27@gmail.com>
2026-01-04 15:42:22 +0100

    fix: return a dictionnary and common config

--- src/config/client.py
+++ src/config/client.py
@@ -3,31 +3,23 @@ import boto3
 from dotenv import load_dotenv
 from src.config.logger import get_logger
 
-def config() -> list:
-    logger = get_logger("client configuration")
+def config() -> dict:
+    logger = get_logger("client-configuration")
     load_dotenv()
     logger.info("loaded dotenv, attempting to create AWS clients")
 
-    clients=[]
+    endpoint = os.getenv("endpoint_url")
+    clients={}
 
     try:
-        s3_client = boto3.client(
-            "s3",
-            endpoint_url=os.getenv("endpoint_url"),
-            aws_access_key_id=os.getenv("aws_access_key_id"),
-            aws_secret_access_key=os.getenv("aws_secret_access_key"),
-            region_name=os.getenv("region_name")
-        )
-        clients.append(s3_client)
-
-        lambda_client = boto3.client(
-            "lambda",
-            endpoint_url=os.getenv("endpoint_url"),
-            aws_access_key_id=os.getenv("aws_access_key_id"),
-            aws_secret_access_key=os.getenv("aws_secret_access_key"),
-            region_name=os.getenv("region_name")
-        )
-        clients.append(lambda_client)
+        common_config = {
+            "endpoint_url": endpoint,
+            "aws_access_key_id": os.getenv("aws_access_key_id"),
+            "aws_secret_access_key": os.getenv("aws_secret_access_key"),
+            "region_name": os.getenv("region_name")
+        }
+        clients["s3"] = boto3.client("s3", **common_config)
+        clients["lambda"] = boto3.client("lambda", **common_config)
         logger.info("successfully created clients")
 
     except Exception as e:
