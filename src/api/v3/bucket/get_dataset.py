6e8edec5f4c7e03b5690a6d4c1b2c092ad0c55da
MathieuAudibert <mathieu.audibert27@gmail.com>
2026-01-06 21:22:22 +0100

    fix: useless tabs

--- src/api/v3/bucket/get_dataset.py
+++ src/api/v3/bucket/get_dataset.py
@@ -7,24 +7,24 @@ router = APIRouter(prefix="/bucket", tags=["buckets"])
 
 @router.get("/dataset")
 async def get_dataset():
-        clients = config()
-        s3_client = clients["s3"]
+    clients = config()
+    s3_client = clients["s3"]
         
-        bucket_name = "local-ml-flow-data"
-        key = "housing.csv"
+    bucket_name = "local-ml-flow-data"
+    key = "housing.csv"
         
-        response = s3_client.get_object(Bucket=bucket_name, Key=key)
-        data = response['Body'].read().decode('utf-8')
+    response = s3_client.get_object(Bucket=bucket_name, Key=key)
+    data = response['Body'].read().decode('utf-8')
                 
-        csv_reader = csv.DictReader(StringIO(data))
-        values = list(csv_reader)
+    csv_reader = csv.DictReader(StringIO(data))
+    values = list(csv_reader)
         
-        return {
-            "dataset_name": "housing.csv",
-            "bucket": bucket_name,
-            "key": key,
-            "row_count": len(values),
-            "values": values
-        }
+    return {
+        "dataset_name": "housing.csv",
+        "bucket": bucket_name,
+        "key": key,
+        "row_count": len(values),
+        "values": values
+    }
     
 
